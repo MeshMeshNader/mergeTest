726d588bf986d3cfd83f5e9b307c729a03410cb3
MeshMeshNader <meshmeshnader555@gmail.com>
2023-05-11 14:43:15 +0300

    Ehsan

--- Messages.java
+++ Messages.java
@@ -5,23 +5,37 @@
  */
 package tictactoeclient;
 
-import java.io.Serializable;
-
 /**
  *
  * @author meshm
  */
-public class Messages implements Serializable{
+public class Messages {
+
     public static String loginResponse = "LoginResponse";
     public static String loginRequest = "LoginRequest";
     public static String registrationResponse = "RegistrationResponse";
     public static String registrationRequest = "RegistrationRequest";
-//    public static String loginResponse = "";
-//    public static String loginResponse = "";
-//    public static String loginResponse = "";
-//    public static String loginResponse = "";
-//    public static String loginResponse = "";
-//    public static String loginResponse = "";
-//    public static String loginResponse = "";
+    public static String getOnlineUsersRequest = "GetOnlineUsersRequest";
+    public static String getOnlineUsersResponse = "GetOnlineUsersResponse";
+    public static String incNumOfWinsRequest = "IncNumOfWinsRequest";
+    public static String incNumOfWinsResponse = "IncNumOfWinsResponse";
+    public static String setGameRequest = "SetGameRequest";
+    public static String setGameResponse = "SetGameResponse ";
+    public static String getGameRequest = "GetGameRequest";
+    public static String getGameResponse = "GetGameResponse";
+    public static String deleteGameRequest = "DeleteGameRequest";
+    public static String deleteGameResponse = "DeleteGameResponse";
+    public static String deleteMoveRequest = "DeleteMoveRequest";
+    public static String deleteMoveResponse = "DeleteMoveResponse";
+    public static String incNumOflossesRequest = "IncNumOflossesRequest";
+    public static String incNumOflossesResponse = "IncNumOflossesResponse";
+    public static String makeUserOnlineRequest = "MakeUserOnlineRequest";
+    public static String makeUserOnlineResponse = "MakeUserOnlineResponse";
+    public static String makeUserOfflineRequest = "MakeUserOfflineRequest";
+    public static String makeUserOfflineResponse = "MakeUserOfflineResponse";
+    public static String getNumberOfWinsRequest = "GetNumberOfWinsRequest";
+    public static String getNumberOfWinsResponse = "GetNumberOfWinsResponse";
+    public static String getNumberOfLossessRequest = "GetNumberOfLossessRequest";
+    public static String getNumberOfLossesResponse = "GetNumberOfLossesResponse";
     
 }
